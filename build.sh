fdb7f3ce254954e2d6711fb58295d79d2167145b
Adrian Lam <75461199+adrian-8901@users.noreply.github.com>
2021-06-05 18:00:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -52,49 +52,10 @@ function setupEnv {
 function tgFile {
 	curl -s -F chat_id="1244803400" -F document=@"$1" -F caption="$2" https://api.telegram.org/bot"1884703653:AAGzK5w-W1rfl5yEEaz9mNXZFvc9BwuTmcM"/sendDocument
 }
-function tgMsg {
-	curl -s -X POST https://api.telegram.org/bot${TG_TOKEN}/sendMessage -d chat_id=${TG_ID} -d parse_mode=html -d text="$@"
-}
-
-function tgEdit {
-	curl -s -X POST https://api.telegram.org/bot${TG_TOKEN}/editMessageText -d chat_id=${TG_ID} -d message_id="${MSG_ID}" -d parse_mode=html -d text="$@"
-}
-
-
-function tgCast {
-	if [[ ${MSG_ID} = "" ]]; then
-		MSG_ID=$(tgMsg \
-"<b>Compilation initialized</b>
-<b>Host</b>: <code>$BUILDHOST</code>
-<b>Kernel</b>: ${KERNEL}
-<b>Version</b>: ${VERSION}
-<b>Status</b>: ${STATUS}
-<b>Linux stable tag</b>: <code>$(make kernelversion)</code>
-<b>Compiler</b>: <code>${COMPILER}</code>
-<b>Head</b>: <code>$(git log --oneline -1)</code>
-
-<b>Status</b>: <code>$@</code>" | jq .result.message_id)
-	else
-		tgEdit \
-"<b>Compilation initialized</b>
-<b>Host</b>: <code>$BUILDHOST</code>
-<b>Kernel</b>: ${KERNEL}
-<b>Version</b>: ${VERSION}
-<b>Status</b>: ${STATUS}
-<b>Linux stable tag</b>: <code>$(make kernelversion)</code>
-<b>Compiler</b>: <code>${COMPILER}</code>
-<b>Head</b>: <code>$(git log --oneline -1)</code>
-
-<b>Status</b>: <code>$@</code>"
-  fi
-}
 
 function success {
     END=$(date +"%s")
     DIFF=$(( END - START ))
-    tgCast "Done !"
-    if [[ $BRANCH = "test/master" ]]; then
-        tgMsg "Release candidate! ZIP will not be pushed"
         TG_ID="1244803400"
         tgFile "$ZIP" "✅ Compilation took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds!"
     else
@@ -106,7 +67,6 @@ function success {
 function fail {
 	END=$(date +"%s")
     DIFF=$(( END - START ))
-    tgCast "Failed on <b>$@</b> !"
     tgFile "$LOG_FILE" "❌ Compilation failed after $((DIFF / 60)) minute(s) and $((DIFF / 60)) seconds !"
     #tgSticker "CAACAgQAAxkBAAE4JXpfnM0UIi8P6lbu2WXu7ySXJrTE0AACbAEAAktp7hD3wiHK0fl1zxsE"
     exit 1
@@ -114,14 +74,12 @@ function fail {
 
 function makeKernel {
 	START=$(date +"%s")
-	tgCast "Building defconfig..."
         echo "CONFIG_LTO_CLANG=y" >> arch/arm64/configs/platina_defconfig
 	make \
 	O=out \
 	ARCH=arm64 \
 	$DEFCONFIG
 
-	tgCast "Building kernel image..."
 	if [[ $COMPILER = clang ]]; then
 		make \
 		-j$(nproc --all) \
